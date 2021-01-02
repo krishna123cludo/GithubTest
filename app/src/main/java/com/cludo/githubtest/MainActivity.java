03310cd840a56906bab52f1f52e50b81cb4ec2c3
krishna123cludo <krishna123cludo@gmail.com>
2021-01-02 17:09:38 +0530

    2nd commit

--- app/src/main/java/com/cludo/githubtest/MainActivity.java
+++ app/src/main/java/com/cludo/githubtest/MainActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+private int i=5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hi
+        for(int
+         i=0;i<5;i++){
+            //logic here
+        }
     }
 }
